b409c3bcb781dbf9d00c98126f04c00786db0b0a
Jack Taylor <taylor.3781@osu.edu>
2025-11-30 23:08:19 -0500

    finished unvectorized, vectorized, and torch implementation of K means clustering

--- utils.py
+++ utils.py
@@ -8,11 +8,11 @@ Functionality to:
 
 """
 
-
 #imports I needed in my homeworks
 from skimage import filters
-from skimage import io, color
+from skimage import io, color, transform
 import numpy as np
+import torch
 from scipy.ndimage import convolve, maximum_filter
 from scipy.linalg import eigh
 import matplotlib.pyplot as plt
@@ -21,26 +21,56 @@ warnings.filterwarnings('ignore') #avoid annoying imshow deprecated
 import math
 from pathlib import Path
 import os
-from typing import List
+from typing import List, Tuple, Optional
 
-#Getting images
-def GET_SAMPLE_IMAGES(p: Path = Path('./sample_images')):
+"""
+Image utils
+"""
+def get_sample_images(p: Path = Path('./sample_images')):
     try: return [io.imread(p / f) for f in os.listdir(p)]
     except: raise ValueError('cannot read sample images')
 
-def PLOT_SAMPLE_IMAGES(images: List):
+def plot_sample_images(images: List):
     try: 
         for img in images: io.imshow(img) ; plt.show()
     except: raise ValueError('cannot plot sample images')
 
-#Distance fns
-def EUCLIDEAN_DIST(p1, p2): return np.linalg.norm(p1 - p2)
-def MANHATTAN_DIST(p1, p2): return np.sum(np.abs(p1 - p2))
-def COS_DIST(p1, p2):
-    dot_prod = np.dot(p1, p2)
-    norm_p1 = np.linalg(p1)
-    norm_p2 = np.linalg(p2)
+def resize_images(images: List, target_size: Optional[Tuple]):
+    return [transform.resize(img, target_size, anti_aliasing=True) for img in images]
 
-    if norm_p1 == 0 or norm_p2 == 0: return 1.0
+"""
+Distance functions. agnostic to torch and numpy
+use compute_distance passing in the desired function in DISTANCE_FN 
+ (supports L1, L2, cos)
+"""
+def euclidean_distance(p1, p2): 
+    if isinstance(p1, torch.Tensor): return torch.norm(p1-p2, dim=-1) #torch
 
-    return 1 - (dot_prod / (norm_p1 * norm_p2))
+    return np.sqrt(np.sum((p1.astype(float)-p2.astype(float))**2, axis=-1))
+
+def manhattan_distance(p1, p2):
+    if isinstance(p1, torch.Tensor): return torch.sum(torch.abs(p1-p2), dim=-1)
+
+    return np.sum(np.abs(p1.astype(float)-p2.astype(float)), axis=-1)
+
+def cosine_distance(p1, p2):
+    torch_obj = isinstance(p1, torch.Tensor)
+
+    dot_prod = (p1*p2).sum(dim=-1) if torch_obj else np.sum(p1*p2, axis=-1)
+    norm_p1 = torch.norm(p1, dim=-1, keepdim=True) if torch_obj else np.linalg.norm(p1, axis=-1, keepdims=False)
+    norm_p2 = torch.norm(p2, dim=-1, keepdim=True) if torch_obj else np.linalg.norm(p2, axis=-1, keepdims=False) 
+
+    return 1 - dot_prod / (norm_p1 * norm_p2 + 1e-8) 
+
+DISTANCE_FNS = {'L1': manhattan_distance, 'L2': euclidean_distance, 'cos': cosine_distance}
+
+def compute_distance(p1, p2, metric): 
+    if isinstance(metric, str):
+        if metric not in DISTANCE_FNS: raise ValueError('invalid distance metric')
+        metric = DISTANCE_FNS[metric]
+
+    return metric(p1,p2)
+
+"""
+other utils ...
+"""
